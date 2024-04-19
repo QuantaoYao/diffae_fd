9e53c86cd02e6311b767184f7419e8ceba98121f
QuantaoYao <2510406365@qq.com>
2024-04-19 17:29:28 +0800

    submit

--- model/separate_model.py
+++ model/separate_model.py
@@ -32,18 +32,9 @@ class separate_model(nn.Module):
         )
 
     def forward(self, criminal_cond, morph_cond):
-        criminal_cond = torch.randn(16, 512)
-        morph_cond = torch.randn(16, 512)
-        criminal_cond_a = self.attn(criminal_cond, morph_cond)
-        accomplice_cond = 2 * morph_cond - criminal_cond_a
+        criminal_cond_attn = self.attn(criminal_cond, morph_cond)
+        accomplice_cond = 2 * morph_cond - criminal_cond_attn
         x = torch.cat([criminal_cond, morph_cond], dim=1)
         x = self.layer(x)
         return accomplice_cond + x
 
-
-if __name__ == '__main__':
-    a = torch.randn(16, 512)
-    b = torch.randn(16, 512)
-    model = separate_model()
-    output = model(a, b)
-    print(output.shape)
--- model/separate_model_ablation_without_attn.py
+++ model/separate_model_ablation_without_attn.py
@@ -32,16 +32,10 @@ class separate_model(nn.Module):
         )
 
     def forward(self, criminal_cond, morph_cond):
-        # criminal_cond_a = self.attn(criminal_cond, morph_cond)
-        # accomplice_cond = 2 * morph_cond -  criminal_cond
+        # criminal_cond_attn = self.attn(criminal_cond, morph_cond)
+        # accomplice_cond = 2 * morph_cond -  criminal_cond_attn
         x = torch.cat([criminal_cond, morph_cond], dim=1)
         x = self.layer(x)
         return x
 
 
-if __name__ == '__main__':
-    a = torch.randn(16, 512)
-    b = torch.randn(16, 512)
-    model = separate_model()
-    output = model(a, b)
-    print(output.shape)
